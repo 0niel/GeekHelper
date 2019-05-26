be8394f71571a9a076fd57b424cd922ad0f1b39a
0niel <51058739+0niel@users.noreply.github.com>
2019-05-26 22:57:59 +0300

    Update GeekHelper.lua

--- GeekHelper.lua
+++ GeekHelper.lua
@@ -15,8 +15,39 @@ encoding.default = 'CP1251'
 u8 = encoding.UTF8
 
 ---------[ МЕСТО ДЛЯ ЛОКАЛЬНЫХ ПЕРЕМЕННЫХ ]--------------------------------
+-----------------------------------------------------------------------------------
+------------------------------- ФИКСЫ ---------------------------------------------
+-----------------------------------------------------------------------------------
 
+-- Фикс зеркального бага alt+tab(черный экран после разворота в инте)
+writeMemory(0x555854, 4, -1869574000, true)
+writeMemory(0x555858, 1, 144, true)
 
+-- функция быстрого прогруза игры. BY KEP4Ik
+function patch()
+	if memory.getuint8(0x748C2B) == 0xE8 then
+		memory.fill(0x748C2B, 0x90, 5, true)
+	elseif memory.getuint8(0x748C7B) == 0xE8 then
+		memory.fill(0x748C7B, 0x90, 5, true)
+	end
+	if memory.getuint8(0x5909AA) == 0xBE then
+		memory.write(0x5909AB, 1, 1, true)
+	end
+	if memory.getuint8(0x590A1D) == 0xBE then
+		memory.write(0x590A1D, 0xE9, 1, true)
+		memory.write(0x590A1E, 0x8D, 4, true)
+	end
+	if memory.getuint8(0x748C6B) == 0xC6 then
+		memory.fill(0x748C6B, 0x90, 7, true)
+	elseif memory.getuint8(0x748CBB) == 0xC6 then
+		memory.fill(0x748CBB, 0x90, 7, true)
+	end
+	if memory.getuint8(0x590AF0) == 0xA1 then
+		memory.write(0x590AF0, 0xE9, 1, true)
+		memory.write(0x590AF1, 0x140, 4, true)
+	end
+end
+patch()
 
 function main()
 	if not isSampLoaded() or not isSampfuncsLoaded() then return end

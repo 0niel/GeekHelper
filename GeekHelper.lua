3e7ecc8004db9c4d3e6563807b3a36ee5f421750
0niel <51058739+0niel@users.noreply.github.com>
2019-05-30 23:30:53 +0300

    About window

--- GeekHelper.lua
+++ GeekHelper.lua
@@ -68,19 +68,10 @@ local win_state = {}
 win_state['update'] = imgui.ImBool(false)
 win_state['main'] = imgui.ImBool(false)
 win_state['settings'] = imgui.ImBool(false)
+win_state['mods'] = imgui.ImBool(false)
 win_state['style'] = imgui.ImBool(false)
+win_state['about'] = imgui.ImBool(false)
 
-
-local SET = {
-	settings = {
-		autologin = false,
-		autogoogle = false,
-		autopass = '',
-		googlekey = '',
-		strobes = false,
-		test = false,
-	}
-}
 
 
 cb1 = imgui.ImBool(false)
@@ -98,6 +89,9 @@ pres = 0
 
 local Config = {
 	ini = {
+		mods = {
+
+		},
 		style = {
 			name = 'Androvira'
 		}
@@ -248,7 +242,9 @@ function Config:Save()
 end
 
 function Config:Load()
+
 	temp = inicfg.load(nil, getGameDirectory()..Config.path)
+	--keyT = imgui.ImBool(Config.ini.mods.keyT)
 	if temp ~= nil then
 		Config.ini = temp
 	else
@@ -257,50 +253,50 @@ function Config:Load()
 end
 
 
---[ImGuiCol_Text] = The color for the text that will be used for the whole menu. 
---[ImGuiCol_TextDisabled] = Color for "not active / disabled text". 
---[ImGuiCol_WindowBg] = Background color. 
---[ImGuiCol_PopupBg] = The color used for the background in ImGui :: Combo and ImGui :: MenuBar. 
---[ImGuiCol_Border] = The color that is used to outline your menu. 
---[ImGuiCol_BorderShadow] = Color for the stroke shadow. 
+--[ImGuiCol_Text] = The color for the text that will be used for the whole menu.
+--[ImGuiCol_TextDisabled] = Color for "not active / disabled text".
+--[ImGuiCol_WindowBg] = Background color.
+--[ImGuiCol_PopupBg] = The color used for the background in ImGui :: Combo and ImGui :: MenuBar.
+--[ImGuiCol_Border] = The color that is used to outline your menu.
+--[ImGuiCol_BorderShadow] = Color for the stroke shadow.
 --[ImGuiCol_FrameBg] = Color for ImGui :: InputText and for background ImGui :: Checkbox
---[ImGuiCol_FrameBgHovered] = The color that is used in almost the same way as the one above, except that it changes color when guiding it to ImGui :: Checkbox. 
---[ImGuiCol_FrameBgActive] = Active color. 
---[ImGuiCol_TitleBg] = The color for changing the main place at the very top of the menu (where the name of your "top-of-the-table" is shown. 
---ImGuiCol_TitleBgCollapsed = ImguiCol_TitleBgActive 
---= The color of the active title window, ie if you have a menu with several windows , this color will be used for the window in which you will be at the moment. 
+--[ImGuiCol_FrameBgHovered] = The color that is used in almost the same way as the one above, except that it changes color when guiding it to ImGui :: Checkbox.
+--[ImGuiCol_FrameBgActive] = Active color.
+--[ImGuiCol_TitleBg] = The color for changing the main place at the very top of the menu (where the name of your "top-of-the-table" is shown.
+--ImGuiCol_TitleBgCollapsed = ImguiCol_TitleBgActive
+--= The color of the active title window, ie if you have a menu with several windows , this color will be used for the window in which you will be at the moment.
 --[ImGuiCol_MenuBarBg] = The color for the bar menu. (Not all sawes saw this, but still)
---[ImGuiCol_ScrollbarBg] = The color for the background of the "strip", through which you can "flip" functions in the software vertically. 
---[ImGuiCol_ScrollbarGrab] = Color for the scoll bar, ie for the "strip", which is used to move the menu vertically. 
---[ImGuiCol_ScrollbarGrabHovered] = Color for the "minimized / unused" scroll bar. 
---[ImGuiCol_ScrollbarGrabActive] = The color for the "active" activity in the window where the scroll bar is located. 
---[ImGuiCol_ComboBg] = Color for the background for ImGui :: Combo. 
---[ImGuiCol_CheckMark] = Color for your ImGui :: Checkbox. 
---[ImGuiCol_SliderGrab] = Color for the slider ImGui :: SliderInt and ImGui :: SliderFloat. 
+--[ImGuiCol_ScrollbarBg] = The color for the background of the "strip", through which you can "flip" functions in the software vertically.
+--[ImGuiCol_ScrollbarGrab] = Color for the scoll bar, ie for the "strip", which is used to move the menu vertically.
+--[ImGuiCol_ScrollbarGrabHovered] = Color for the "minimized / unused" scroll bar.
+--[ImGuiCol_ScrollbarGrabActive] = The color for the "active" activity in the window where the scroll bar is located.
+--[ImGuiCol_ComboBg] = Color for the background for ImGui :: Combo.
+--[ImGuiCol_CheckMark] = Color for your ImGui :: Checkbox.
+--[ImGuiCol_SliderGrab] = Color for the slider ImGui :: SliderInt and ImGui :: SliderFloat.
 --[ImGuiCol_SliderGrabActive] = Color of the slider,
---[ImGuiCol_Button] = the color for the button. 
---[ImGuiCol_ButtonHovered] = Color when hovering over the button. 
---[ImGuiCol_ButtonActive] = Button color used. 
---[ImGuiCol_Header] = Color for ImGui :: CollapsingHeader. 
---[ImGuiCol_HeaderHovered] = Color, when hovering over ImGui :: CollapsingHeader. 
---[ImGuiCol_HeaderActive] = Used color ImGui :: CollapsingHeader. 
---[ImGuiCol_Column] = Color for the "separation strip" ImGui :: Column and ImGui :: NextColumn. 
---[ImGuiCol_ColumnHovered] = Color, when hovering on the "strip strip" ImGui :: Column and ImGui :: NextColumn. 
+--[ImGuiCol_Button] = the color for the button.
+--[ImGuiCol_ButtonHovered] = Color when hovering over the button.
+--[ImGuiCol_ButtonActive] = Button color used.
+--[ImGuiCol_Header] = Color for ImGui :: CollapsingHeader.
+--[ImGuiCol_HeaderHovered] = Color, when hovering over ImGui :: CollapsingHeader.
+--[ImGuiCol_HeaderActive] = Used color ImGui :: CollapsingHeader.
+--[ImGuiCol_Column] = Color for the "separation strip" ImGui :: Column and ImGui :: NextColumn.
+--[ImGuiCol_ColumnHovered] = Color, when hovering on the "strip strip" ImGui :: Column and ImGui :: NextColumn.
 --[ImGuiCol_ColumnActive] = The color used for the "separation strip" ImGui :: Column and ImGui :: NextColumn.
---[ImGuiCol_ResizeGrip] = The color for the "triangle" in the lower right corner, which is used to increase or decrease the size of the menu. 
---[ImGuiCol_ResizeGripHovered] = Color, when hovering to the "triangle" in the lower right corner, which is used to increase or decrease the size of the menu. 
---[ImGuiCol_ResizeGripActive] = The color used for the "triangle" in the lower right corner, which is used to increase or decrease the size of the menu. 
---[ImGuiCol_CloseButton] = The color for the button-closing menu. 
---[ImGuiCol_CloseButtonHovered] = Color, when you hover over the button-close menu. 
+--[ImGuiCol_ResizeGrip] = The color for the "triangle" in the lower right corner, which is used to increase or decrease the size of the menu.
+--[ImGuiCol_ResizeGripHovered] = Color, when hovering to the "triangle" in the lower right corner, which is used to increase or decrease the size of the menu.
+--[ImGuiCol_ResizeGripActive] = The color used for the "triangle" in the lower right corner, which is used to increase or decrease the size of the menu.
+--[ImGuiCol_CloseButton] = The color for the button-closing menu.
+--[ImGuiCol_CloseButtonHovered] = Color, when you hover over the button-close menu.
 --[ImGuiCol_CloseButtonActive] = The color used for the button-closing menu.
---[ImGuiCol_TextSelectedBg] = The color of the selected text, in ImGui :: MenuBar. 
---[ImGuiCol_ModalWindowDarkening] = The color of the "Blackout Window" of your menu. 
---I rarely see these designations, but still decided to put them here. 
---[ImGuiCol_Tab] = The color for tabs in the menu. 
+--[ImGuiCol_TextSelectedBg] = The color of the selected text, in ImGui :: MenuBar.
+--[ImGuiCol_ModalWindowDarkening] = The color of the "Blackout Window" of your menu.
+--I rarely see these designations, but still decided to put them here.
+--[ImGuiCol_Tab] = The color for tabs in the menu.
 --[ImGuiCol_TabActive] = The active color of tabs, ie when you click on the tab you will have this color.
---[ImGuiCol_TabHovered] = The color that will be displayed when hovering on the table. 
---[ImGuiCol_TabSelected] = The color that is used when you are in one of the tabs. 
---[ImGuiCol_TabText] = Text color that only applies to tabs. 
+--[ImGuiCol_TabHovered] = The color that will be displayed when hovering on the table.
+--[ImGuiCol_TabSelected] = The color that is used when you are in one of the tabs.
+--[ImGuiCol_TabText] = Text color that only applies to tabs.
 --[ImGuiCol_TabTextActive] = Active text color for tabs.
 
 writeMemory(0x555854, 4, -1869574000, true)
@@ -349,6 +345,7 @@ function main()
 		imgui.Process = win_state['main'].v
 		wait(0)
 	end
+
 end
 
 function mainmenu()
@@ -356,7 +353,7 @@ function mainmenu()
 end
 
 
-function updateCheck() 
+function updateCheck()
 	local zapros = https.request("https://geekhub.pro/samp/geekhelper/version.json")
 
 	if zapros ~= nil then
@@ -406,7 +403,7 @@ local menubar_font = nil
 local fa_glyph_ranges = imgui.ImGlyphRanges({ fa.min_range, fa.max_range })
 function imgui.BeforeDrawFrame()
 	if fa_font == nil then
-		local font_config = imgui.ImFontConfig() 
+		local font_config = imgui.ImFontConfig()
 		font_config.MergeMode = true
 
 		fa_font = imgui.GetIO().Fonts:AddFontFromFileTTF('moonloader/GeekHelper/files/fontawesome-webfont.ttf', 14.0, font_config, fa_glyph_ranges)
@@ -415,15 +412,15 @@ function imgui.BeforeDrawFrame()
 end
 
 
-function async_http_request(method, url, args, resolve, reject) 
+function async_http_request(method, url, args, resolve, reject)
 	local request_lane = lanes.gen('*', {package = {path = package.path, cpath = package.cpath}}, function()
 		local requests = require 'requests'
 		local ok, result = pcall(requests.request, method, url, args)
 		if ok then
-			result.json, result.xml = nil, nil 
+			result.json, result.xml = nil, nil
 			return true, result
 		else
-			return false, result 
+			return false, result
 		end
 	end)
 	if not reject then reject = function() end end
@@ -446,9 +443,9 @@ function async_http_request(method, url, args, resolve, reject)
 end
 
 function imgui.OnDrawFrame()
-	local tLastKeys = {} 
-	local sw, sh = getScreenResolution() 
-	local btn_size = imgui.ImVec2(-0.1, 0) 
+	local tLastKeys = {}
+	local sw, sh = getScreenResolution()
+	local btn_size = imgui.ImVec2(-0.1, 0)
 	local btn_size2 = imgui.ImVec2(160, 0)
 	local btn_size3 = imgui.ImVec2(140, 0)
 
@@ -473,23 +470,85 @@ function imgui.OnDrawFrame()
 	imgui.PopStyleVar()
 
 	--imgui.ShowCursor = win_state['update'].v
-	if win_state['main'].v then 
+	if win_state['main'].v then
 		imgui.SetNextWindowPos(imgui.ImVec2(sw / 2, sh / 2), imgui.Cond.FirstUseEver, imgui.ImVec2(0.5, 0.5))
 		imgui.SetNextWindowSize(imgui.ImVec2(280, 250), imgui.Cond.FirstUseEver)
 
 		imgui.Begin(' GeekHelper', win_state['main'], imgui.WindowFlags.NoResize)
-		
+
 		if imgui.Button(u8' Настройки', btn_size) then
 			--print("каракули")
 			win_state['settings'].v = not win_state['settings'].v
 		end
-
+		if imgui.Button(u8' Модификации', btn_size) then
+			--print("каракули")
+			win_state['mods'].v = not win_state['mods'].v
+		end
+		if imgui.Button(u8' О скрипте', btn_size) then
+			--print("каракули")
+			win_state['about'].v = not win_state['about'].v
+		end
 		if imgui.Button('Style', btn_size) then
 			win_state['style'].v = not win_state['style'].v
 		end
 		imgui.End()
 	end
-	if win_state['settings'].v then 
+	if win_state['mods'].v then
+		imgui.Begin(u8'Модификации', win_state['mods'])
+
+		--imgui.AlignTextToFramePadding(); imgui.Text(u8(" Чат на клавишу Т")); imgui.SameLine(); imgui.Checkbox(u8'Чат на клавишу T', keyT)
+
+		imgui.Spacing()
+		imgui.Separator()
+		imgui.Spacing()
+
+		if imgui.Button('button') then pres = pres + 1 end
+
+		imgui.BeginGroup()
+		imgui.Text('group')
+		imgui.EndGroup()
+
+		imgui.Columns(3, 'cols', true)
+
+		imgui.Text('bullet')
+		imgui.Bullet()
+
+		imgui.Checkbox('checkbox1', cb1)
+
+		imgui.Bullet()
+		imgui.Checkbox('checkbox2', cb2)
+
+		imgui.RadioButton('radio1', rb, 0)
+		imgui.RadioButton('radio2', rb, 1)
+
+		imgui.Combo('combo', c, {'item1', 'item2', 'item3'}, 3)
+
+		imgui.InputText('inputtext', it)
+		imgui.Text('text: '..it.v)
+
+		imgui.DragFloat('dragfloat', df, imgui.ImFloat(0.001), 0, 100, '%.3f', imgui.ImFloat(0.001))
+
+		imgui.SliderFloat('sliderfloat', sf, 0, 100, '%.3f', imgui.ImFloat(0.001))
+
+		imgui.Selectable('selectable1', false, 0, imgui.ImVec2(70, 20))
+		imgui.Selectable('selectable2', s, 0, imgui.ImVec2(70, 20))
+		text = 'selected'
+		if not s.v then text = 'not '..text end
+		imgui.Text(text)
+
+		imgui.ListBox('listbox', lb, {'item1', 'item2', 'item3'})
+
+		imgui.NextColumn()
+		imgui.Text('nextcolumn: ')
+		imgui.ColorEdit4('coloredit4', f4)
+
+		imgui.NextColumn()
+		imgui.Text('button pressed: '..tostring(pres))
+		imgui.ColorPicker4('colorpicker4', f4)
+
+		imgui.End()
+	end
+	if win_state['settings'].v then
 		imgui.Begin(u8'Настройки', win_state['settings'])
 
 		imgui.Text('text')
@@ -516,12 +575,12 @@ function imgui.OnDrawFrame()
 
 		imgui.RadioButton('radio1', rb, 0)
 		imgui.RadioButton('radio2', rb, 1)
-		
+
 		imgui.Combo('combo', c, {'item1', 'item2', 'item3'}, 3)
 
 		imgui.InputText('inputtext', it)
 		imgui.Text('text: '..it.v)
-		
+
 		imgui.DragFloat('dragfloat', df, imgui.ImFloat(0.001), 0, 100, '%.3f', imgui.ImFloat(0.001))
 
 		imgui.SliderFloat('sliderfloat', sf, 0, 100, '%.3f', imgui.ImFloat(0.001))
@@ -537,7 +596,7 @@ function imgui.OnDrawFrame()
 		imgui.NextColumn()
 		imgui.Text('nextcolumn: ')
 		imgui.ColorEdit4('coloredit4', f4)
-		
+
 		imgui.NextColumn()
 		imgui.Text('button pressed: '..tostring(pres))
 		imgui.ColorPicker4('colorpicker4', f4)
@@ -549,28 +608,55 @@ function imgui.OnDrawFrame()
 		imgui.ShowStyleEditor()
 		imgui.End()
 	end
-	if win_state['update'].v then 
+	if win_state['about'].v then -- окно "о скрипте"
+		imgui.SetNextWindowPos(imgui.ImVec2(sw/2, sh/2), imgui.Cond.FirstUseEver, imgui.ImVec2(0.5, 0.5))
+		imgui.SetNextWindowSize(imgui.ImVec2(360, 225), imgui.Cond.FirstUseEver)
+		imgui.Begin(u8('О скрипте'), win_state['about'], imgui.WindowFlags.NoResize)
+
+		imgui.Text(u8'GeekHelper - игровой помощник')
+		imgui.Text(u8'Разработчики: Oniel & CzarAlex')
+		imgui.Text(u8'Версия скрипта: '..thisScript().version)
+		imgui.Separator()
+		if imgui.Button(u8'VK.Oniel') then
+			sampAddChatMessage("[GeekHelper]{FFFFFF} Сейчас откроется ссылка ВКонтакте.", 0x046D63)
+			print(shell32.ShellExecuteA(nil, 'open', 'https://vk.com/onie1', nil, nil, 1))
+		end
+		imgui.SameLine()
+		if imgui.Button(u8'VK.CzarAlex') then
+			sampAddChatMessage("[GeekHelper]{FFFFFF} Сейчас откроется ссылка ВКонтакте.", 0x046D63)
+			print(shell32.ShellExecuteA(nil, 'open', 'https://vk.com/czar.alex', nil, nil, 1))
+		end
+		imgui.SameLine()
+		if imgui.Button(u8'Перезагрузить скрипт', btn_size) then
+			thisScript():reload()
+		end
+		if imgui.Button(u8'Отключить скрипт', btn_size) then
+			thisScript():unload()
+		end
+		imgui.End()
+	end
+	if win_state['update'].v then
 		imgui.SetNextWindowPos(imgui.ImVec2(sw/2, sh/2), imgui.Cond.FirstUseEver, imgui.ImVec2(0.5, 0.5))
 		imgui.SetNextWindowSize(imgui.ImVec2(450, 200), imgui.Cond.FirstUseEver)
 
 		imgui.Begin(u8('Обновление'), nil, imgui.WindowFlags.NoResize)
 
 		imgui.Text(u8'Обнаружено обновление до версии: '..updatever)
-		
+
 		imgui.Separator()
-		
+
 		imgui.TextWrapped(u8("Для установки обновления необходимо подтверждение пользователя, разработчик настоятельно рекомендует принимать обновления ввиду того, что прошлые версии через определенное время отключаются и более не работают."))
-		
+
 		if imgui.Button(u8'Скачать и установить обновление', btn_size) then
 			async_http_request('GET', 'https://geekhub.pro/samp/geekhelper/GeekHelper.lua', nil,
-				function(response) 
+				function(response)
 				local f = assert(io.open(getWorkingDirectory() .. '/GeekHelper.lua', 'wb'))
 				f:write(response.text)
 				f:close()
 				sampAddChatMessage("[GeekHelper]{FFFFFF} Обновление успешно, перезагружаем скрипт.", 0x046D63)
 				thisScript():reload()
 			end,
-			function(err) 
+			function(err)
 				print(err)
 				sampAddChatMessage("[GeekHelper]{FFFFFF} Произошла ошибка при обновлении, попробуйте позже.", 0x046D63)
 				win_state['update'].v = not win_state['update'].v
